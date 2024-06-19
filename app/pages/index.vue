db02c26589761487072dea366f8136e813ebf42c
Rajeev R Sharma <i.rarsh@gmail.com>
2024-06-19 22:18:18 +0530

    feat: test UI for making calls

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,26 @@
 <template>
-  <main>WELCOME</main>
+  <main>
+    <h1>WELCOME</h1>
+
+    <div>
+      <input v-model="toNumber" placeholder="No. with country code" />
+      <button @click="requestCallback">Request a callback</button>
+    </div>
+  </main>
 </template>
+
+<script setup lang="ts">
+const toNumber = ref("");
+const requestCallback = async () => {
+  if (toNumber.value) {
+    const call = await $fetch("/api/call", {
+      method: "POST",
+      body: { to: toNumber.value },
+    });
+
+    console.log("call api res", call);
+  } else {
+    alert("Please enter your number");
+  }
+};
+</script>
